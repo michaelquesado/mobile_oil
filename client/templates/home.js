496f063f0f48babe24a00254b080db2bca85956d
michaelquesado <esquesado@gmail.com>
2015-06-20 15:20:39 -0300

    ajustes na listagem dos postos.

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,21 +1,22 @@
+var url_s = "http://places.api.here.com/places/v1/discover/search?at=-7.3063696,-39.2946653&q=petrol-station&app_id=hG4gnJyrmlbNgGscL7Ki&app_code=h3XG36Nr4RgQOjymUTblJQ&pretty";
+    
+     HTTP.call("GET", url_s,function (err,result){
+
+        console.log(JSON.parse(result.content).results.items);
+        Session.set('places', JSON.parse(result.content).results.items);
+        
+});
+
 Template.home.helpers({
  
   lat: function() { return Geolocation.latLng().lat; },
   lng: function(){ return Geolocation.latLng().lng;  },
-  listaDePostos: function() {
-  	
-  	var url_s = "http://places.api.here.com/places/v1/discover/search?at="+Geolocation.latLng().lat+","+Geolocation.latLng().lng+"&q=petrol-station&app_id=hG4gnJyrmlbNgGscL7Ki&app_code=h3XG36Nr4RgQOjymUTblJQ&pretty&callback=PostoPlaces&Accept-Language=de";
-  	console.log(url_s);
-  	 HTTP.call("GET", url_s,function (err,result){
 
- 	 		console.log(result.content);
-  	 		Session.set('places', result.content);
-  	 		
-  	 });
- 
-  	 		return Session.get('places');
-		
-    },
+    ListaTodosPostos: function(){
+
+         return Session.get('places');
+    }
+	 
 });
 
 //mobileoilh5m1@gmail.com	
