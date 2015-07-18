2ac7418374e245a6644b0f04df2d495fbc247637
michaelquesado <esquesado@gmail.com>
2015-07-18 18:23:54 -0300

    coreção do bug ao adicionar as preferencias.

--- client/templates/preferencias.js
+++ client/templates/preferencias.js
@@ -14,59 +14,39 @@ Template.preferencias.helpers({
 
 });
 
-var toggle = 1;
+
+var preferencias = new Array();	
 var i = 0;
-var preferencias = new Array();
+
 
 Template.preferencias.events({
 
-	'click  li': function(event){
 
-		var active_class = ' active ';
-		
-		toggle++;
-		var id =  event.target.id;
 
-		if(toggle % 2 == 0){
 
-			$(event.target).find('#toggle'+ event.target.id).addClass(active_class);					
-			
-			
-			if(preferencias.indexOf(id) == -1){
-				preferencias[i] = id;
+	'submit' : function(event){
+
+		event.preventDefault();
+
+		$.each($(":checkbox"), function(){
+			if(this.checked){
+				preferencias[i] = this.value;
 				i++;
 			}
+		});
 
-		}else{
-
-			$(event.target).find('#toggle'+ event.target.id).removeClass(active_class);
-			$.each(preferencias, function(k,v){
-				if(v == id){
-					delete preferencias[k];
-				}
-			});
-		}
-
-
-		console.log(preferencias);
-		Session.set('preferencias', preferencias);
-		return false;
-	},
-
-	'click button ': function(event){
-
-		var preferencias = Session.get('preferencias');
 		var person = Session.get('person');
-
-
+		
 		Meteor.call('CadastrarPreferenciasDeCombustivel',
 			person.id,
 			preferencias,
 			function(err,result){
-				console.log(result.content);
+				
+				(JSON.parse(result.content) === 'sucesso')? Router.go('/home') : alert (result.content);
 			}
-		);
-	}
+			);
+	},
+
 
 	
 
