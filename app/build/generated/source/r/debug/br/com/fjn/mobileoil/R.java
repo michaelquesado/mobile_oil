e9956cf6555c83f3654a325c691cdbb0e66ebe26
JuniorGuita <junior.siqueira.ti@gmail.com>
2015-08-26 11:14:40 -0300

    Verificacao do GPS ainda na primeira tela, com bug e sem classe separada

--- app/build/generated/source/r/debug/br/com/fjn/mobileoil/R.java
+++ app/build/generated/source/r/debug/br/com/fjn/mobileoil/R.java
@@ -874,10 +874,8 @@ containing a value of this type.
         public static final int app_name=0x7f0b000e;
         public static final int hello_world=0x7f0b000f;
         public static final int title_activity_combustivel=0x7f0b0010;
-        public static final int title_activity_novo_preco=0x7f0b0011;
-        public static final int title_activity_posto_combustivel_detalhes=0x7f0b0012;
-        public static final int title_activity_preferencias=0x7f0b0013;
-        public static final int title_activity_sobre=0x7f0b0014;
+        public static final int title_activity_posto_combustivel_detalhes=0x7f0b0011;
+        public static final int title_activity_preferencias=0x7f0b0012;
     }
     public static final class style {
         /**  Customize your theme here. 
--- app/src/main/java/br/com/fjn/mobileoil/MainActivity.java
+++ app/src/main/java/br/com/fjn/mobileoil/MainActivity.java
@@ -1,17 +1,32 @@
 package br.com.fjn.mobileoil;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity implements View.OnClickListener,LocationListener {
     private Button mEntrarFacebook;
     private Button mEntrarGooglePlus;
     private Button mEntrarSemCadastro;
+
+    private String TAG = "-----------------------------------";
+    private LocationManager locationManager;
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +40,20 @@ public class MainActivity extends Activity implements View.OnClickListener {
         mEntrarFacebook.setOnClickListener(this);
         mEntrarGooglePlus.setOnClickListener(this);
         mEntrarSemCadastro.setOnClickListener(this);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, true);
+        Log.i(TAG, "-------------------------------------"+ provider);
+        Location location = locationManager.getLastKnownLocation(provider);
+        Log.i(TAG, "-------------------------------------"+ location);
+        if(location !=null){
+            Log.i(TAG, "------------------------------------------------- Entrou no If");
+            Log.i(TAG, "Provider :" + provider + " foi selecionado ");
+        }else{
+            Log.i(TAG, "------------------------------------------------- ENtrou no else");
+            onProviderDisabled(provider);
+        }
     }
 
     @Override
@@ -58,5 +87,63 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public void onClick(View v) {
         Intent it = new Intent(this, PreferenciasActivity.class);
         startActivity(it);
+    }
+
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.i(TAG, "Chamou o metodo onResume");
+        super.onResume();
+        locationManager.requestLocationUpdates(provider, 0, 0, this);
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.i(TAG, "Chamou o metodo onPause");
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i(TAG, "Chamou o metodo onLocationChange");
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        Log.i(TAG, "----------------------------------------------"+lat);
+        Log.i(TAG, "----------------------------------------------"+lng);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.i(TAG, "Chamou o metodo onStatusChanged");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.i(TAG, "Chamou o metodo onProviderEnable");
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.i(TAG, "Chamou o metodo onProviderDiseble");
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("O GPS está desligado, deseja ativa-lo agora?").setCancelable(false).setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                Intent callGpsSettingIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(callGpsSettingIntent);
+            }
+        });
+        alertDialogBuilder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+                Toast.makeText(getApplicationContext(), "Para usar o APP é preciso ativar GPS!!!", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
     }
 }
